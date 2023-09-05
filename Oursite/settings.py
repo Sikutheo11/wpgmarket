e8e6687b284046b929c3f352b28bdca036c5c4cf
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2023-09-05 05:20:02 -0800

    kkkkk

--- Oursite/settings.py
+++ Oursite/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT =BASE_DIR/'static'
 STATICFILES_DIRS =[
-    'oursite/static'
+    'static'
 ]
 #media file configuration
 STATIC_URL = '/media/'
